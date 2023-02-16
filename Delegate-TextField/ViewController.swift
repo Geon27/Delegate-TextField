fcd8fcfecc6ee31c36e00d9aee606db53d08f47c
Geon_27 <yukgeon98527@gmail.com>
2023-02-16 09:04:33 +0900

    23/02/16 keyboard setting in Viewcontroller.swift

--- Delegate-TextField/ViewController.swift
+++ Delegate-TextField/ViewController.swift
@@ -11,10 +11,11 @@ class ViewController: UIViewController {
     @IBOutlet var tf: UITextField!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 텍스트 필드 속성 설정
+        self.tf.placeholder = "값을 입력하세요" // 안내 메시지
+        self.tf.keyboardType = UIKeyboardType.alphabet // 키보드 타입 영문자 패드로
+        self.tf.keyboardAppearance = UIKeyboardAppearance.dark // 키보드 스타일 어둡게
+        self.tf.returnKeyType = UIReturnKeyType.join // 리턴키 타입은 "Join"
+        self.tf.enablesReturnKeyAutomatically = true // 리턴키 자동 활성화 "True"
     }
-
-
 }
-
